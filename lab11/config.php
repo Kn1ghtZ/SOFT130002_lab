c5c607b958acf7a82cfb7ad36966ab29326fd501
luyongyong <596406242@qq.com>
2020-06-13 10:49:21 +0800

    lab11

---
+++ lab11/config.php
@@ -0,0 +1,8 @@
+<?php
+define('DBHOST', 'localhost');
+define('DBNAME', 'login');
+define('DBUSER', 'testuser');
+define('DBPASS', '331234567qwe');
+define('DBCONNSTRING','mysql:host=localhost;dbname=login');
+
+?>
